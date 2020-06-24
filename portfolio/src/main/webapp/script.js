3d5144a109c8e850977f26f1311f41fa10bb1a7d
meghakak <meghakak1@gmail.com>
2020-06-24 01:11:51 +0000

    Add jQuery code to script.js and connect html and js files.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/** jQuery - Allow access of navbar.html to other files. */
 $(document).ready(function() {
-    $('#top-navigation').load('navbar.html');
+  $('#top-navigation').load('navbar.html');
 });
 
 function addRandomFunFact() {

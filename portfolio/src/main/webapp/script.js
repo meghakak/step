1f32a6bcb5875b846b55b04b9970981bd07a1932
meghakak <meghakak1@gmail.com>
2020-06-24 00:44:46 +0000

    Separate common code to limit repetitive html code.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+$(document).ready(function() {
+    $('#top-navigation').load('navbar.html');
+});
+
 function addRandomFunFact() {
   const funFacts =
       ['I am a Black Belt in Mixed Martial Arts 🥋', 

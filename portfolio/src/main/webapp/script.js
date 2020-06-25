c81a1579260333d7e67c5ae7dc0ac9bdb245d8a6
meghakak <meghakak1@gmail.com>
2020-06-25 23:12:32 +0000

    Add gallery intro caption and fix spacing.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -65,10 +65,10 @@ function showSlides(index) {
 
   // Hide all slides except the current one
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" dot-active", "");
+    dots[i].className = dots[i].className.replace(" dot-active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " dot-active";

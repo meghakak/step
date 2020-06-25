ce5f3f42da0bc612ecdf5f85768718515d5cca73
meghakak <meghakak1@gmail.com>
2020-06-25 21:09:05 +0000

    Add slideshow template from w3schools.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -32,3 +32,33 @@ function addRandomFunFact() {
   const funFactContainer = document.getElementById('fun-fact-container');
   funFactContainer.innerText = funFact;
 }
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/** Slideshow - next and previous controls */
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/** Slideshow - display image */
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" dot-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " dot-active";
+}

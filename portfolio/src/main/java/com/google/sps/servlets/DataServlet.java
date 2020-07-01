0d5068350acf0e8edcf57870c88ae47a838a9e94
meghakak <meghakak1@gmail.com>
2020-07-01 19:30:12 +0000

    Add and load comments from Datastore.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,11 +14,16 @@
 
 package com.google.sps.servlets;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import java.util.Map;
-import java.util.HashMap;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -26,22 +31,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// TODO: Find a way to only allow unique usernames (but returning users should be able to write additional comments)
+// TODO: Only allow unique usernames - returning users should still be able to write additional comments with the same username
 /** Servlet that returns inputted fun facts from users and stores previously inputted facts. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   private static final String USER_NAME_KEY = "user-name";
   private static final String FUN_FACT_KEY = "user-fun-fact";
   private static final String NO_NAME = "Anonymous";
-  private Map<String, String> oldFacts = new HashMap<String, String>();
+  private static final String ENTITY_NAME = "FunFact";
+  private static final String PROPERTY_NAME = "name";
+  private static final String PROPERTY_FACT = "fact";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String oldFactsJson = convertToJsonUsingGson(oldFacts);
+    Query query = new Query(ENTITY_NAME).addSort(PROPERTY_FACT, SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    // Add previously inputted fun facts to userFacts
+    Map<String, String> userFacts = new HashMap<String, String>();
+    for (Entity entity : results.asIterable()) {
+      String name = (String) entity.getProperty(PROPERTY_NAME);
+      String fact = (String) entity.getProperty(PROPERTY_FACT);
+
+      if (userFacts.containsKey(name)) {
+        String newValue = userFacts.get(name) + "; " + fact;
+        userFacts.remove(name);
+        userFacts.put(name, newValue);
+      }
+      else {
+        userFacts.put(name, fact);
+      }
+    }
+    String userFactsJson = convertToJsonUsingGson(userFacts);
 
     // Send the JSON as the response
     response.setContentType("application/json;");
-    response.getWriter().println(oldFactsJson);
+    response.getWriter().println(userFactsJson);
   }
 
   private static String convertToJsonUsingGson(Map<String, String> oldFacts) {
@@ -62,22 +89,17 @@ public class DataServlet extends HttpServlet {
       return;
     }
 
-    // Define key and value pair to add to oldFacts
-    String factKey = getFactKey(userName);
-    String factValue = getFactValue(oldFacts, userFunFact, factKey);
-    
-    // Store user's input to access later
-    oldFacts.put(factKey, factValue);
-    Entity funFactEntity = new Entity("FunFact");
-    funFactEntity.setProperty("name", factKey);
-    funFactEntity.setProperty("fact", factValue);
- 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    // Define key to add to datastore
+    String factKey = getFactKey(userName);
+
+    Entity funFactEntity = new Entity(ENTITY_NAME);
+    funFactEntity.setProperty(PROPERTY_NAME, factKey);
+    funFactEntity.setProperty(PROPERTY_FACT, userFunFact);
     datastore.put(funFactEntity);
 
-    // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
-    response.getWriter().println(factKey + ": " + factValue);
   }
 
   private static String getFactKey(String userName) {
@@ -89,17 +111,6 @@ public class DataServlet extends HttpServlet {
       factKey = userName;
     }
     return factKey;
-  }
-
-  private static String getFactValue(Map<String, String> oldFacts, String funFact, String factKey) {
-    String factValue;
-    if (oldFacts.containsKey(factKey)) {
-      factValue = oldFacts.get(factKey) + "; " + funFact;
-    }
-    else {
-      factValue = funFact;
-    }
-    return factValue;
   }
 
   private static String getParameter(HttpServletRequest request, String content, String defaultValue) {

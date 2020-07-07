a58cdb0b4555dc4f089d246ea92bae5b758654ba
meghakak <meghakak1@gmail.com>
2020-07-07 15:50:39 +0000

    Remove license comments and edit stream statements.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -1,24 +1,9 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Streams;
-import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -55,7 +40,8 @@ public class DataServlet extends HttpServlet {
 
     // Add previously inputted fun facts to userFacts
     ImmutableList<String> userFacts =
-        Streams.stream(results.asIterable(FetchOptions.Builder.withLimit(commentsLimit)))
+        Streams.stream(results.asIterable())
+        .limit(commentsLimit)
         .map(DataServlet::getContent)
         .collect(toImmutableList());
 

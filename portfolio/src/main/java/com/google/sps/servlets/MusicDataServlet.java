0d7f3eb308954b1635805769a30f5138d2230892
meghakak <meghakak1@gmail.com>
2020-07-08 18:39:06 +0000

    Add helper functions and switch var-const as needed.

--- portfolio/src/main/java/com/google/sps/servlets/MusicDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MusicDataServlet.java
@@ -6,8 +6,6 @@ import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
 import com.google.common.collect.Streams;
 import com.google.appengine.api.datastore.FetchOptions;
@@ -33,20 +31,33 @@ public class MusicDataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("application/json");
     Query query = new Query(ENTITY_NAME);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-    // Add previously inputted fun facts to userFacts
+    // Add datastore entities with votes for each genre
     ImmutableMap<String, Integer> genreVotes =
         Streams.stream(results.asIterable())
-            .collect(toImmutableMap(entity -> entity.getProperty(PROPERTY_GENRE).toString(), entity -> Integer.parseInt(entity.getProperty(PROPERTY_VOTES).toString())));
+            .collect(toImmutableMap(MusicDataServlet::getEntityKey, MusicDataServlet::getEntityValue));
 
+    String genreVotesJson = convertToJsonUsingGson(genreVotes);
+    response.setContentType("application/json");
+    response.getWriter().println(genreVotesJson);
+  }
+
+  private static String getEntityKey(Entity entity) {
+    return entity.getProperty(PROPERTY_GENRE).toString();
+  }
+
+  private static Integer getEntityValue(Entity entity) {
+    return Integer.parseInt(entity.getProperty(PROPERTY_VOTES).toString());
+  }
+
+  private static String convertToJsonUsingGson(ImmutableMap<String, Integer> genreVotes) {
     Gson gson = new Gson();
     String json = gson.toJson(genreVotes);
-    response.getWriter().println(json);
+    return json;
   }
 
   @Override
@@ -67,7 +78,6 @@ public class MusicDataServlet extends HttpServlet {
       genreEntity.setProperty(PROPERTY_VOTES, Integer.parseInt(genreEntity.getProperty(PROPERTY_VOTES).toString())+1);
     }
     datastore.put(genreEntity);
-
     response.sendRedirect("/about-you.html");
   }
 }
